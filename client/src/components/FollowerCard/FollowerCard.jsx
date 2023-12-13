e949d6334c1d9c2960b8fca41ea9ebc92ff027da
Rejoy01 <rejoywilson65@gmail.com>
2023-12-13 16:45:59 +0530

    followerCard

---
+++ client/src/components/FollowerCard/FollowerCard.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./FollowerCard.css";
+import { Followers } from "../../Data/FollowersData";
+
+const FollowerCard = () => {
+  return (
+    <div className="FollowerCard">
+      <h3>Who is following you</h3>
+      {Followers.map((follower, id) => {
+        return (
+          <div className="follower">
+
+            <div>
+
+              <img src={follower.img} alt="No image" className="followerImg" />
+              <div className="name">
+                <span>{follower.name}</span>
+                <span>@{follower.username}</span>
+              </div>
+                
+            </div>
+            <button className="button">follow</button>
+            
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default FollowerCard;
--- client/src/components/ProfileSide/ProfileSide.jsx
+++ client/src/components/ProfileSide/ProfileSide.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import LogoSearch from '../LogoSearch/LogoSearch'
 import ProfileCard from '../ProfileCard/ProfileCard'
 import "./ProfileSide.css"
+import FollowerCard from '../FollowerCard/FollowerCard'
 
 const ProfileSide = () => {
   return (
    <div className="profileSide">
         <LogoSearch />
         <ProfileCard />
+        <FollowerCard />
 
 
    </div>
