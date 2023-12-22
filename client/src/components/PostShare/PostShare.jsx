8680079393027f0be6f2b61629b3fd6fa62a9137
Rejoy01 <rejoywilson65@gmail.com>
2023-12-22 18:10:08 +0530

    postShare

--- client/src/components/PostShare/PostShare.jsx
+++ client/src/components/PostShare/PostShare.jsx
@@ -5,6 +5,7 @@ import { UilScenery } from "@iconscout/react-unicons";
 import { UilPlayCircle } from "@iconscout/react-unicons";
 import { UilLocationPoint } from "@iconscout/react-unicons";
 import { UilSchedule } from "@iconscout/react-unicons";
+import { UilTimes } from "@iconscout/react-unicons";
 
 const PostShare = () => {
 
@@ -13,6 +14,9 @@ const PostShare = () => {
   const onImageChange =(event)=>{
     if(event.target.files && event.target.files[0]){
       let img = event.target.files[0];
+      setImage({
+        image: URL.createObjectURL(img)
+      })
     }
     
   }
@@ -24,7 +28,7 @@ const PostShare = () => {
         <input type="text" placeholder="What's happening" />
         <div className="postOptions">
           <div className="option" style={{ color: "var(--photo)" }}
-          >
+            onClick={()=>imageRef.current.click()}>
             <UilScenery />
             Photo
           </div>
@@ -45,6 +49,13 @@ const PostShare = () => {
           <input type="file" name="myImage" ref={imageRef} onChange={onImageChange} />
         </div>
         </div>
+
+          {image && (
+            <div className="previewImage">
+                <UilTimes onClick={()=>setImage(null)}/>
+                <img src={image.image} alt="" />
+            </div>
+          )}
 
       </div>
     </div>
