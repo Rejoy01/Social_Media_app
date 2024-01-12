854e1794ab1d8a61baf6c4212ec2b088f006b0ea
Rejoy01 <rejoywilson65@gmail.com>
2024-01-12 19:31:31 +0530

    Signup

--- client/src/pages/auth/Auth.jsx
+++ client/src/pages/auth/Auth.jsx
@@ -1,21 +1,44 @@
-import React from 'react'
-import "./Auth.css"
-import Logo from "../../img/logo.png"
-
+import React from "react";
+import "./Auth.css";
+import Logo from "../../img/logo.png";
 
 const Auth = () => {
   return (
-    <div className='Auth'>
+    <div className="Auth">
       <div className="a-left">
         <img src={Logo} alt="" />
         <div className="Webname">
-            <h1>Travel Media</h1>
-            <h6>Explore the ideas throughout the World</h6>
+          <h1>Travel Media</h1>
+          <h6>Explore the ideas throughout the World</h6>
         </div>
       </div>
-      <h1>Form</h1>
+      <SignUp />
     </div>
-  )
+  );
+};
+
+function SignUp() {
+  return (
+    <div className="a-right">
+      <form className="infoForm">
+        <h3>Sign up</h3>
+        <div>
+          <input
+            type="text"
+            className="infoInput"
+            placeholder="Firstname"
+            name="firstname"
+          />
+          <input
+            type="text"
+            className="infoInput"
+            placeholder="LastName"
+            name="lastname"
+          />
+        </div>
+      </form>
+    </div>
+  );
 }
 
-export default Auth
+export default Auth;
