f26060392b660a9c1c240e743ca455793175d3af
Rejoy01 <rejoywilson65@gmail.com>
2024-01-18 18:00:04 +0530

    ProfileModel

--- client/src/components/profileModel/ProfileModel.jsx
+++ client/src/components/profileModel/ProfileModel.jsx
@@ -15,9 +15,31 @@ function ProfileModel({setModalOpened,modalOpened}) {
           blur: 3,
           
         }}
+        size='55%'
       >
         <form className='infoForm'>
             <h3>You Info</h3>
+            <div>
+                <input type="text" className="infoInput" name='FirstName' placeholder='First Name'/>
+                <input type="text" className="infoInput" name='LastName' placeholder='Last Name'/>
+            </div>
+            <div>
+            <input type="text" className="infoInput" name='WorksAt' placeholder='Works At'/>
+            </div>
+            <div>
+            <input type="text" className="infoInput" name='LivesIn' placeholder='Lives In'/>
+            <input type="text" className="infoInput" name='Country' placeholder='Country'/>
+            </div>
+            <div>
+            <input type="text" className="infoInput" name='RelationStatus' placeholder='RelationShip Status'/>
+            </div>
+            <div>
+                Profile Image
+                <input type="file" name='profileImg' />
+                Cover Image
+                <input type="file" name='coverImg' />
+            </div>
+            <button className='button infoButton'>Update</button>
 
         </form>
       </Modal>
