d0ba5b3125df1d99485f83afde132393c08d7c02
Rejoy01 <rejoywilson65@gmail.com>
2024-02-04 09:46:19 +0530

    followController

--- server/Routes/UserRoutes.js
+++ server/Routes/UserRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { GetUser, deleteUser, updateUser } from "../controller/UserController.js";
+import { GetUser, deleteUser, followUser, updateUser } from "../controller/UserController.js";
 
 const router = express.Router()
 
@@ -10,5 +10,6 @@ router.get("/",async(req,res)=>{
 router.get('/:id',GetUser)
 router.put('/:id',updateUser)
 router.delete('/:id',deleteUser)
+router.put('/:id/follow',followUser)
 
 export default router
--- server/controller/UserController.js
+++ server/controller/UserController.js
@@ -59,3 +59,30 @@ export const deleteUser = async (req , res ) => {
         res.status(403).send('Access Denied ! you can only update your own profile')
     }
 }
+
+//Follow a User 
+export const followUser = async(req,res) =>{
+    const id = req.params.id
+
+    const { currentUserId} = req.body
+
+    if (currentUserId ===id) {
+        res.status(403).json("Action Forbidden")
+    }
+    else{
+        try {
+            const followUser = await UserModel.findById(id)
+            const followingUser = await UserModel.findById(currentUserId)
+            if (!followUser.followers.includes(currentUserId)) {
+                await followUser.updateOne({$push :{followers:currentUserId}})
+                await followingUser.updateOne({$push :{following:id}})
+                res.status(200).json("User Followed")
+            }else{
+                res.status(403).json("User is Already followed by you")
+            }
+
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    }
+}
