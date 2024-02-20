448bfc66feecdf0e0efc1e286ffc8099ee5a5ab3
Rejoy01 <rejoywilson65@gmail.com>
2024-02-20 10:26:03 +0530

    Deletepost

--- server/controller/PostController.js
+++ server/controller/PostController.js
@@ -48,3 +48,18 @@ export const  updatePost = async(req,res)=>{
     }
 }
 
+export const deletePost =async(req,res)=>{
+    const id= req.params.id;
+    const {userId} = req.body
+    try {
+        const post = await PostModel.findById(id)
+        if (post.userId === userId) {
+            await post.deleteOne();
+        }else{
+            res.status(403).json("Action forbidden")
+        }
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
+
